4c3c706644606155c06d663737ac49fcf9863fc2
Dillow <madushanka20021224@gmail.com>
2026-03-07 19:29:32 +0530

    improve loging ui

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,27 +1,76 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
+
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(title: Text("Login")),
       body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: "Email"),
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Password"),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Login"),
-            )
-          ],
+        padding: const EdgeInsets.all(24),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Text(
+                "TaskFlow",
+                style: TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              Text(
+                "Smart Task Manager",
+                style: TextStyle(fontSize: 16),
+              ),
+
+              SizedBox(height: 40),
+
+              TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              TextField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+
+              SizedBox(height: 30),
+
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Login"),
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              TextButton(
+                onPressed: () {},
+                child: Text("Create Account"),
+              )
+
+            ],
+          ),
         ),
       ),
     );
